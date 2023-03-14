e9ac043cf71df3edbdacadf18a173485a75b557f
6ladeus6 <cladeus@cesde.net>
2023-03-13 19:52:45 -0500

    Taller

--- Ejercicio#7.js
+++ Ejercicio#7.js
@@ -1,12 +1,16 @@
-let miarray = Array.from({length: 20}, () => Math.floor(Math.random() * -10));
-let contador = 0
+let miarray = Array.from({length: 20}, () => Math.floor(Math.random() * 10));
 
 function clasificarNumeros() {
     for (let index = 0; index < miarray.length; index++) {
-        const element = array[index];
-        if (miarray[index] < 0) {
-            
+        console.log(miarray[index])
+        if (miarray[index] < 0.5) {
+            console.log("El mas cercano a cero es: ", miarray[index])
+        }else if (miarray[index]< 0){
+
+            console.log(Math.abs(miarray[index]))
+
         }
         
     }
 }
+clasificarNumeros()
